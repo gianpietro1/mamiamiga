95a576615e95485d173faaa4ddceb587fe13692e
Gianpietro Lavado <gianpietro1@gmail.com>
2016-05-08 16:10:28 -0500

    first commercial version

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module Mamiamiga
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.i18n.default_locale = :es
 
+    config.assets.paths << Rails.root.join('assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
