e14f0e257f2639006357a5b8afdf6f613f102a3a
Gianpietro Lavado <gianpietro1@gmail.com>
2016-07-23 11:45:00 -0500

    image max size to 10MB

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -11,6 +11,7 @@ Ckeditor.setup do |config|
   # By default: %w(jpg jpeg png gif tiff)
   # config.image_file_types = %w(jpg jpeg png gif tiff)
 
+  config.swf_file_size_limit = "10 MB"
   # Allowed flash file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
